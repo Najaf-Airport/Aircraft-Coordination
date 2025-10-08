e724f2ec204f6385a6758fdd3ffed85ca4a36b4c
Najaf Airport <bvbvff923@gmail.com>
2025-10-08 18:01:17 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,14 +2,14 @@
 
 // ضع هنا معلومات مشروعك من Firebase
 const firebaseConfig = {
-   apiKey: "AIzaSyAENB1CxP4gRmqGfi71Fq2y9O6cBzSN13U",
-    authDomain: "najafairportsite.firebaseapp.com",
-    projectId: "najafairportsite",
-    storageBucket: "najafairportsite.firebasestorage.app",
-    messagingSenderId: "855371683567",
-    appId: "1:855371683567:web:d1d393c054d17993dfad1a",
-    measurementId: "G-Y7341CEWGT"
-  };
+  apiKey: "AIzaSyAENB1CxP4gRmqGfi71Fq2y9O6cBzSN13U",
+  authDomain: "najafairportsite.firebaseapp.com",
+  projectId: "najafairportsite",
+  storageBucket: "najafairportsite.appspot.com",
+  messagingSenderId: "855371683567",
+  appId: "1:855371683567:web:d1d393c054d17993dfad1a",
+  measurementId: "G-Y7341CEWGT"
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
